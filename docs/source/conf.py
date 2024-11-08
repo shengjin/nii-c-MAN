530ce4783b33e6dd4a30d568bca904fc81bb1ef1
jins <sheng.s.jin@gmail.com>
2024-11-08 16:37:43 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Nii-C'
+copyright = '2024, Sheng JIN'
+author = 'Sheng JIN'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.1'
+version = '1.1.0'
 
 # -- General configuration
 
